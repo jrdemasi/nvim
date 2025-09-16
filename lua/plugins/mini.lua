6f507732dcebec5466396372e1144864ff3d36c5
Jonathan DeMasi <jonathan.demasi@resy.com>
2025-09-15 21:31:03 -0600

    formatting

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,10 +1,9 @@
 return {
-	{
-		'nvim-mini/mini.nvim', 
-		version = false,
-		config = function(_, opts)
-			require('mini.statusline').setup()
-			-- require('mini.files').setup()
-		end,
-},
+  {
+    'nvim-mini/mini.nvim', 
+    version = false,
+    config = function(_, opts)
+      require('mini.statusline').setup()
+    end,
+  },
 }
