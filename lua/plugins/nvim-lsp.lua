806d00eeaf2dbb7fcef3aadba943d782c1c25e00
Jonathan DeMasi <jonathan.demasi@resy.com>
2025-09-15 21:56:16 -0600

    add full tf support

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -2,9 +2,11 @@ return {
   {
     "https://github.com/neovim/nvim-lspconfig",
     config = function()
-    local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
       lspconfig.gopls.setup({})
       lspconfig.pyright.setup({})
+      require'lspconfig'.terraformls.setup{}
+      require'lspconfig'.tflint.setup{}
     end,
   }
 }
