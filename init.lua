2f10cfc2884e499d140ba2aca48248c3a356fbdc
Jonathan DeMasi <jrdemasi@gmail.com>
2025-09-16 10:13:32 -0600

    Update formatting init.lua

--- init.lua
+++ init.lua
@@ -3,9 +3,9 @@ vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', {no
 -- vim.keymap.set('n', '<leader>f', '<cmd>lua MiniFiles.open()<cr>', { desc = "Find files recursively" })
 vim.keymap.set('n', '<leader>f', '<cmd>Oil --float . <cr>', { desc = "Find files recursively" })
 vim.diagnostic.config({
-    virtual_text = true, -- Display diagnostics as inline text
-    -- Other options can be added here, like:
-    signs = false,       -- Show icons in the gutter
-    underline = true,    -- Highlight the affected text
+  virtual_text = true, -- Display diagnostics as inline text
+  -- Other options can be added here, like:
+  signs = false,       -- Show icons in the gutter
+  underline = true,    -- Highlight the affected text
 })
 vim.cmd 'colorscheme atom-dark-256'
