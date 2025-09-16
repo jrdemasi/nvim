0b10b18a3845b36589377ffcced9c3423c9f0429
Jonathan DeMasi <jonathan.demasi@resy.com>
2025-09-15 21:38:04 -0600

    formatting

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,24 +1,24 @@
 return {
-	{
-		'nvim-telescope/telescope.nvim', 
-		tag = '0.1.8',
-      		dependencies = { 'nvim-lua/plenary.nvim', 
-      			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-      		},
-		config = function()
-			require('telescope').setup {
-				pickers = {
-					find_files = {
-						theme = "ivy"
-					}
-				},
-				extensions = {
-					fzf = {}
-				}
-			}
-
-			require('telescope').load_extension('fzf')
-			vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
-	end
+  {
+    'nvim-telescope/telescope.nvim', 
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim', 
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
+    config = function()
+      require('telescope').setup {
+	pickers = {
+	  find_files = {
+	    theme = "ivy"
+	  }
+	},
+	extensions = {
+	  fzf = {}
+	}
       }
+
+      require('telescope').load_extension('fzf')
+      vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+    end
+  }
 }
