91ced784452f7c3ceb7a569201db7e8607246f15
Jonathan DeMasi <jrdemasi@gmail.com>
2025-09-18 22:20:40 -0600

    update to new format

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -2,11 +2,10 @@ return {
   {
     "https://github.com/neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.gopls.setup({})
-      lspconfig.pyright.setup({})
-      require'lspconfig'.terraformls.setup{}
-      require'lspconfig'.tflint.setup{}
+      vim.lsp.enable('pyright')
+      vim.lsp.enable('gopls')
+      vim.lsp.enable('terraformls')
+      vim.lsp.enable('tflint')
     end,
   }
 }
